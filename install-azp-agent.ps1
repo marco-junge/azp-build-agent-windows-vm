e9318c0084e68ebe762bdaf34d8bca00bb417f15
Marco Junge <marco-junge@gmx.de>
2020-01-01 18:05:41 +0100

    Format script

--- install-azp-agent.ps1
+++ install-azp-agent.ps1
@@ -1,31 +1,31 @@
 [CmdletBinding()]
 Param
 (
-	[Parameter(Mandatory=$true)]
-	[string]$AzpAccount,
+  [Parameter(Mandatory = $true)]
+  [string]$AzpAccount,
 
-	[Parameter(Mandatory=$true)]
-	[string]$PersonalAccessToken,
+  [Parameter(Mandatory = $true)]
+  [string]$PersonalAccessToken,
 
-	[Parameter(Mandatory=$false)]
+  [Parameter(Mandatory = $false)]
   [string]$PoolName = "Default",
 
-	[Parameter(Mandatory=$false)]
-	[string]$AgentName = ${Env:computername},
+  [Parameter(Mandatory = $false)]
+  [string]$AgentName = ${Env:computername},
   
-  [Parameter(Mandatory=$false)]
+  [Parameter(Mandatory = $false)]
   [string]$AgentInstallationPath = 'C:\Azure-Pipelines-Agent',
-  
-  [Parameter(Mandatory=$false)]
-	[string]$AgentWorkPath = 'C:\Azure-Pipelines-Agent\_work',
 
-	[Parameter(Mandatory=$false)]
-	[int]$downloadMaxRetries = 5,
+  [Parameter(Mandatory = $false)]
+  [string]$AgentWorkPath = 'C:\Azure-Pipelines-Agent\_work',
 
-	[Parameter(Mandatory=$false)]
-	[int]$downloadRetrySleepSeconds = 15,
+  [Parameter(Mandatory = $false)]
+  [int]$downloadMaxRetries = 5,
 
-	[boolean]$prerelease=$false
+  [Parameter(Mandatory = $false)]
+  [int]$downloadRetrySleepSeconds = 15,
+
+  [boolean]$PreRelease = $false
 )
 
 $currentLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
@@ -40,27 +40,24 @@ $agentTempDownloadFilePath = "$agentTempFolderName\azp-agent.zip"
 $retries = 1
 Write-Host "Downloading Azure Pipelines Agent install files."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-do
-{
-  try
-  {
+do {
+  try {
     Write-Verbose "Trying to get download URL for latest azure pipelines agent release..."
     $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/azure-pipelines-agent/releases"
-	  $latestRelease = $latestRelease | Where-Object prerelease -eq $prerelease | where-object assets -ne $null | Sort-Object created_at -Descending | Select-Object -First 1
+    $latestRelease = $latestRelease | Where-Object prerelease -eq $PreRelease | where-object assets -ne $null | Sort-Object created_at -Descending | Select-Object -First 1
     $assetsURL = ($latestRelease.assets).browser_download_url
     $latestReleaseDownloadUrl = ((Invoke-RestMethod -Uri $assetsURL) -match 'win-x64').downloadurl
     Invoke-WebRequest -Uri $latestReleaseDownloadUrl -Method Get -OutFile "$agentTempDownloadFilePath"
     Write-Verbose "Downloaded agent successfully on attempt $retries" -verbose
     break
   }
-  catch
-  {
+  catch {
     $exceptionText = ($_ | Out-String).Trim()
     Write-Verbose "Exception occured downloading agent: $exceptionText in try number $retries" -verbose
     $retries++
     Start-Sleep -Seconds $downloadRetrySleepSeconds
   }
-} 
+}
 while ($retries -le $downloadMaxRetries)
 
 Expand-Archive -Path "$agentTempDownloadFilePath" -DestinationPath "$AgentInstallationPath" -Force
@@ -80,9 +77,9 @@ $AzpUrl = "https://dev.azure.com/$AzpAccount"
   --url "$AzpUrl" `
   --auth PAT `
   --token "$PersonalAccessToken" `
-	--pool "$PoolName" `
-	--runAsService `
-	--work "$AgentWorkPath" `
+  --pool "$PoolName" `
+  --runAsService `
+  --work "$AgentWorkPath" `
   --replace
 
 Pop-Location
